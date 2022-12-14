60a00b79b3439595eb66dcbb8a610599ce7b59ff
Louis Pires <louisppires@gmail.com>
2022-12-14 10:00:24 +0200

    Added readline

--- Program.cs
+++ Program.cs
@@ -1,30 +1,42 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
-int counter = 0;  
+int counter = 0;
 StringBuilder sb = new StringBuilder();
-  
-// Read the file and display it line by line.  
-foreach (string line in System.IO.File.ReadLines(@"C:\Users\louis\Downloads\WhatsApp Chat with John Snow.txt"))
-{  
-    if (line.Length > 10 && line.Substring(4, 1).Equals(@"/"))
-    {
-        string sDate = line.Substring(0, 10);
-        string rest = line.Substring(10, line.Length - 10);
 
-        string newLine = $"{sDate.Substring(8, 2)}/{sDate.Substring(5, 2)}/{sDate.Substring(0, 4)}{rest}";
-        sb.AppendLine(newLine);
-    }
-    else
-        sb.Append(line);
+System.Console.WriteLine(@"Enter the path of the Whatsapp Chat you would like to fix the date for:");
 
-    counter++;  
-}  
+string? path = System.Console.ReadLine();
+string? filename = Path.GetFileNameWithoutExtension(path);
+string? directory = Path.GetDirectoryName(path);
 
-using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\louis\Downloads\WhatsApp Chat with John Snow - DateFixed.txt"))
+
+if (!string.IsNullOrEmpty(path))
 {
-    file.WriteLine(sb.ToString());
+    // Read the file and display it line by line.  
+    foreach (string line in System.IO.File.ReadLines(path))
+    {
+        if (line.Length > 10 && line.Substring(4, 1).Equals(@"/"))
+        {
+            string sDate = line.Substring(0, 10);
+            string rest = line.Substring(10, line.Length - 10);
+
+            string newLine = $"{sDate.Substring(8, 2)}/{sDate.Substring(5, 2)}/{sDate.Substring(0, 4)}{rest}";
+            sb.AppendLine(newLine);
+        }
+        else
+            sb.AppendLine(line);
+
+        counter++;
+    }
+
+    using (System.IO.StreamWriter file = new System.IO.StreamWriter(Path.Combine(directory, filename + " - DateFixed.txt")))
+    {
+        file.WriteLine(sb.ToString());
+    }
 }
 
-System.Console.WriteLine("There were {0} lines.", counter);  
+System.Console.WriteLine("There were {0} lines.", counter);
+System.Console.WriteLine("New file contains {0} lines.", Regex.Matches(sb.ToString(), Environment.NewLine).Count);
 // Suspend the screen.  
-System.Console.ReadLine();  
+System.Console.ReadLine();
